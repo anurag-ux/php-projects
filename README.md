6296418ba8538db03d7adc6c9d0e3e959626086c
Anurag Shandilya <kinganurag8@gmail.com>
2020-09-03 18:06:07 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # php-projects
+<img src="https://www.designologix.in/wp-content/uploads/2016/05/php-logo.png">
+<br>
+This contains some files of me practicing php.
+<hr>
+
+# Usage
+- Clone the repositary
+```
+    git clone https://github.com/anurag-ux/php-projects.git`
+```
+- go to the cloned folder and start the php-server.
+```
+    cd php-projects
+    php -S localhost:4000 
+```
+- thats it! now you can run any project just by typing in your browser
+```
+    http://localhost:4000/{filename.php}
+ ```
+# Enjoy!
--- calculator.php
+++ calculator.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,7 +8,7 @@
 <body>
     <a href="http://localhost:4000/calculator.php">Homepage</a>
     <br><br>
-    <form action="calculator.php" method="get">
+    <form action="calculator.php" method="post">
         Number 1 : <input type="number" name="num1" placeholder="enter number">
         <br><br>
         Number 2 : <input type="number" name="num2" placeholder="enter number">
@@ -17,8 +17,9 @@
     </form>
     Answer: 
     <?php
-        $num1=$_GET["num1"];
-        $num2=$_GET["num2"];
+        $a=array();
+        $num1=$_POST["num1"];
+        $num2=$_POST["num2"];
         echo $num1+$num2;
     ?>
 </body>
