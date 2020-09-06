72313d74270f60e87a2c57f0e2a1115090543e5d
Anurag <kinganurag8@gmail.com>
2020-09-07 01:44:25 +0530

    added forms

---
+++ form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <a href="http://localhost:4000/form.php">Homepage</a>
+    <form action="form.php" method="post">
+        username: <input type="text" name="username">
+        password: <input type="password" name="password">
+        <input type="submit">
+    </form>
+    <?php
+        $username=$_POST["username"];
+        $password=$_POST["password"];
+        echo("$username");
+    ?>
+</body>
+</html>
